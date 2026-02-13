ac8237897ee4314b3b6cd110ef548871a13ec8e5
DracoBlue <JanS@DracoBlue.de>
2026-02-13 21:19:16 +0100

    Added possibility to select openai compatible model/server

--- src/llm.ts
+++ src/llm.ts
@@ -2,14 +2,17 @@ import { hermesToolMiddleware } from "@ai-sdk-tool/parser";
 import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
 import { addToolInputExamplesMiddleware, extractReasoningMiddleware, wrapLanguageModel, type LanguageModel } from "ai";
 
+const OPENAI_COMPATIBLE_MODEL_ID = process.env.OPENAI_COMPATIBLE_MODEL_ID ?? 'qwen-local';
+const OPENAI_COMPATIBLE_BASE_URL = process.env.OPENAI_COMPATIBLE_BASE_URL ?? 'http://localhost:8000/v1';
+
 // --- MODEL SETUP ---
 const localProvider = createOpenAICompatible({
-  name: 'mlx',
-  baseURL: 'http://localhost:8000/v1',
+  name: 'local',
+  baseURL: OPENAI_COMPATIBLE_BASE_URL,
 });
 
 export const model : LanguageModel = process.env.AI_GATEWAY_MODEL_ID ? process.env.AI_GATEWAY_MODEL_ID : wrapLanguageModel({
-  model: localProvider.languageModel('qwen-local'),
+  model: localProvider.languageModel(OPENAI_COMPATIBLE_MODEL_ID),
   middleware: [
     hermesToolMiddleware,
     addToolInputExamplesMiddleware({
